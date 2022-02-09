b10f75700381aae86d6a4fd9afb8a1ab2c61b77f
OscarDeGar <odelagarza@olin.edu>
2022-02-09 17:45:57 -0500

    Updated func. for motor and sensor test

--- TestCode/initMotors/initMotors.ino
+++ TestCode/initMotors/initMotors.ino
@@ -1,12 +1,32 @@
-int motorPin = 3;
+int motor1 = 2;
+int motor2 = 3;
+int motor3 = 4;
+int motor4 = 5;
+int motor5 = 6;
+int motor6 = 7;
 
 void setup() {
-  pinMode(motorPin, OUTPUT);
+  pinMode(motor1, OUTPUT);
+  pinMode(motor2, OUTPUT);
+  pinMode(motor3, OUTPUT);
+  pinMode(motor4, OUTPUT);
+  pinMode(motor5, OUTPUT);
+  pinMode(motor6, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(motorPin, HIGH); // sets the digital pin 13 on
-  delay(10000);            // waits for a second
-  digitalWrite(motorPin, LOW);  // sets the digital pin 13 off
-  delay(1000);            // waits for a second
+  digitalWrite(motor1, HIGH); // sets the digital pin 2 on
+  digitalWrite(motor2, HIGH); // sets the digital pin 3 on
+  digitalWrite(motor3, HIGH); // sets the digital pin 4 on
+  digitalWrite(motor4, HIGH); // sets the digital pin 5 on
+  digitalWrite(motor5, HIGH); // sets the digital pin 6 on
+  digitalWrite(motor6, HIGH); // sets the digital pin 7 on
+  delay(10000);            // run for 10 seconds
+  digitalWrite(motor1, LOW); // sets the digital pin 2 off
+  digitalWrite(motor2, LOW); // sets the digital pin 3 off
+  digitalWrite(motor3, LOW); // sets the digital pin 4 off
+  digitalWrite(motor4, LOW); // sets the digital pin 5 off
+  digitalWrite(motor5, LOW); // sets the digital pin 6 off
+  digitalWrite(motor6, LOW); // sets the digital pin 7 off
+  delay(3000);            // waits for 3 seconds
 }
--- TestCode/initSensors/initSensors.ino
+++ TestCode/initSensors/initSensors.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+// LIST OF ALL SENSORS WILL UPDATE LATER ON
+int encoderPin = A0;   // encoderPin
+// int photoPin = 13;   `// photoResistor pin
+int sensorValue = 0;  // stores encoder value
 
+
+void setup() {
+  // declare the photoPin as an OUTPUT:
+//  pinMode(photoPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // read the value from the sensor:
+  sensorValue = analogRead(encoderPin);
+//  // turn the ledPin on
+//  digitalWrite(ledPin, HIGH);
+//  // stop the program for <sensorValue> milliseconds:
+//  delay(sensorValue);
+//  // turn the ledPin off:
+//  digitalWrite(ledPin, LOW);
+//  // stop the program for for <sensorValue> milliseconds:
+//  delay(sensorValue);
 }
